8228095ede67c3af390d091e71acffa6054e2d77
Claudiu Plesa <claudiu.plesa@magicpixel.ro>
2022-04-10 11:23:31 +0300

    Add icons

---
+++ src/lib-components/breezzyio/main-icon.vue
@@ -0,0 +1,31 @@
+<script>
+
+import { defineComponent } from 'vue';
+
+// import IconTenants from './icons/IconTenants.vue'
+// import IconReviews from './icons/IconReviews.vue'
+// import IconCompany from './icons/IconCompany.vue'
+
+
+// export default {
+//      name: 'main-icon',
+//      props:['component'],
+//      components: {
+//           IconTenants,
+//           IconReviews,
+//           IconCompany
+//      },
+// }
+
+
+export default /*#__PURE__*/defineComponent({
+  name: 'Breezzyio/mainIcon', // vue component name
+});
+
+
+</script>
+
+
+<template>
+     <component v-bind:is="component" class="w-12 h-12 text-icongray flex-shrink-0"/>
+</template>
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as Breezzyio/saasUiSample } from './breezzyio/saas-ui-sample.vue';
+export { default as Breezzyio/mainIcon } from './breezzyio/main-icon.vue';
